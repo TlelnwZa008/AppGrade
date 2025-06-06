338e3e64b9d56c0d6bfbc500ddff3fb9438d3d08
344_04 <344_04@DESKTOP-PJP3DQL.mshome.net>
2025-06-06 14:36:40 +0700

    first 1

---
+++ src/main/java/com/mycompany/appgrade/AppGrade.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.appgrade;
+
+/**
+ *
+ * @author 344_04
+ */
+public class AppGrade {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
